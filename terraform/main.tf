1dd0b78d8ae00d1ccb3f0a2a182e47a8d0d4ff3a
Sewon Kim <pswk0907@gmail.com>
2026-02-05 08:09:17 +0900

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+# public ip 자동 할당(associate_public_ip_address) 기본값 true이기 때문에 생략
+
+# region
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+# ami
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  owners = ["amazon"]
+}
+
+# terraform resource
+resource "aws_instance" "k3s_node" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+  key_name      = "infra-dev-key" # key pair
+  vpc_security_group_ids = [aws_security_group.sg_7th_room.id] # sc
+
+  tags = {
+    Name = "dev-k3s-node-01"
+    Environment = "dev"
+    Project = "infra-auto"
+  }
+}
