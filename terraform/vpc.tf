9ff54a2fa2d1932386c86fe9790547284fb9391a
Sewon Kim <pswk0907@gmail.com>
2026-02-05 08:09:33 +0900

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,12 @@
+# public subn 수동 지정
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
