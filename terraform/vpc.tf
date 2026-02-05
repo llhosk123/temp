e5f90676cfdcddc1f72ab215a7d0c252782b068d
Sewon Kim <pswk0907@gmail.com>
2026-02-05 14:00:21 +0900

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,13 +1,17 @@
-# public subn 수동 지정
-# 여러 인스턴스 생성 시 필요
-
+# VPC 조회
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "default" {
+# public subnet 수동 지정
+data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "map-public-ip-on-launch"
+    values = ["true"]
   }
 }
