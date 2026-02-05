2a858dda802a3f14926724ee76b15bbc013d4aef
Sewon Kim <pswk0907@gmail.com>
2026-02-05 13:59:21 +0900

    Update sg.tf

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -2,7 +2,7 @@
 # 외부 공개 포트: 80/443
 # 내부/관리 포트: 22, 3000, 6443 -> VPC 내부만 허용
 
-# 왜 sg를 두 개로 나눈 것?
+# sg 2개 분리해서 붙이는 구조 (관리용(SSH) / 서비스용(웹/클러스터) 역할 분리)
 
 # sg 생성
 resource "aws_security_group" "sg_7th_room" {
@@ -42,7 +42,14 @@ resource "aws_security_group" "web-sg" {
     protocol    = "tcp"
     # cidr_blocks = ["10.0.0.0/16"] # 내부에서 오는 트래픽 허용? 기본 VPC랑 범위가 안맞음
     # cidr_blocks = ["0.0.0.0/0"] # 실무에서는 외부 전체 허용이 표준
-    cidr_blocks = [data.aws_vpc.default.cidr_block] # VPC 내부 공격 시연 시 (실습용)
+    cidr_blocks = [data.aws_vpc.default.cidr_block] # VPC 내부 공격 시연 시 (실습용) - 외부 웹 서비스 불가
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [data.aws_vpc.default.cidr_block]
   }
 
   # K3s API 서버
@@ -51,7 +58,7 @@ resource "aws_security_group" "web-sg" {
     to_port         = 6443
     protocol        = "tcp"
     # security_groups = [aws_security_group.sg_7th_room.id] # ?
-    self            = true # 같은 sg끼리 모든 통신 허용 (K3s 노드들이 같은 sg 사용) -> 우리는 EC2 하나니까 자기 자신 통신만 허용
+    self            = true # 같은 sg끼리 모든 통신 허용 (K3s 노드들이 같은 sg 사용) -> 우리는 EC2 하나니까 자기 자신 통신만 허용 (EC2가 1대로 안할거면 수정해야 함)
   }
 
   # K3s Flannel CNI (노드 간 모든 통신 허용)
