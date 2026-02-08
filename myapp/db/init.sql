56d367c24d828924474ab4b8534e7df1581d9d3e
llhosk <138557428+llhosk123@users.noreply.github.com>
2026-02-08 19:12:38 +0900

    Update admins table schema and insert new records

--- myapp/db/init.sql
+++ myapp/db/init.sql
@@ -1,12 +1,20 @@
 CREATE DATABASE IF NOT EXISTS admin_db;
-
 USE admin_db;
 
-CREATE TABLE IF NOT EXISTS admins (
-    admin_id VARCHAR(20) PRIMARY KEY,
-    admin_pw VARCHAR(60) NOT NULL
+DROP TABLE IF EXISTS admins;
+CREATE TABLE admins (
+    admin_id VARCHAR(50) PRIMARY KEY,
+    password_hash VARCHAR(255) NOT NULL
 );
 
-INSERT INTO admins (admin_id, admin_pw) VALUES
-('admin01', 'Init@Admin01!'),
-('admin02', 'Init@Admin02!');
+INSERT INTO admins (admin_id, password_hash) VALUES
+('admin', SHA2('ad12@@3', 256)),
+('master', SHA2('ma555#', 256)),
+('leader', SHA2('ldr445!', 256)),
+('security', SHA2('secu369@', 256)),
+('db_admin', SHA2('dadm@511', 256)),
+('k3s_manager', SHA2('km2253@', 256)),
+('infra_manager', SHA2('ifm1246!', 256)),
+('db_manager', SHA2('24maks5', 256)),
+('infra_admin', SHA2('aozk311@', 256)),
+('k3s_admin', SHA2('rkskekfk216', 256));
